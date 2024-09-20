27c00aa2385592279ed12af369c1ab87eb43ec64
hourai <hourai12450@gmail.com>
2024-09-20 16:43:40 +0800

    icon图标组件的编写

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div>
     <TopNav/>
     <SwiperTop/>
+    <IconList/>
   </div>
 </template>
 
@@ -10,10 +11,11 @@
 
 import TopNav from "@/components/home/TopNav.vue"
 import SwiperTop from "@/components/home/SwiperTop.vue"
+import IconList from "@/components/home/IconList.vue"
 export default {
   name: 'HomeView',
   components: {
-    TopNav,SwiperTop
+    TopNav,SwiperTop,IconList,
   }
 }
 </script>
